9871abf9382f747770a624311bae72ab1560d227
bkfwebdev <bryant.feld@gmail.com>
2021-09-20 13:05:14 -0400

    some styling added

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function PoKecard (props) {
     return (
-        <div>
+        <div className = "Pokecard">
              <p>Name:{props.name}</p>
              <img src = {`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`}/>
              <p>Type:{props.type}</p>
